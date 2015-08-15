ca1ffa4f22828f30776dcd44c73ba67b237875f4
Juliusz Chroboczek <jch@pps.univ-paris-diderot.fr>
2015-08-16 00:27:03 +0200

    Fix sending of capabilities.

--- send.c
+++ send.c
@@ -200,12 +200,12 @@ format_my_state(unsigned char *buf, int buflen)
     CHECK(11);
     SHORT(32);
     SHORT(12);
+    SHORT(0x100);
     if(serve_dhcpv4) {
-        SHORT(0x104);               /* Version 1, L = 4 */
+        SHORT(4);               /* L = 4 */
     } else {
-        SHORT(0x100);
+        SHORT(0);
     }
-    SHORT(0);
     BYTES("SHNCPD/0", 8);
     PAD();
 
